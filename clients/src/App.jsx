09dbc230a04d3d0dddae99252f25644fad0a0dbb
trinhhoangminh200401 <76164749+trinhhoangminh200401@users.noreply.github.com>
2023-05-28 13:51:31 +0700

    Delete clients directory

--- clients/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Directory from "./Directory";
-import Categories from './data/categories.json' 
-const App = () => {
- 
-  return (
-   <Directory categories={Categories} />
-  );
-};
-export default App;
