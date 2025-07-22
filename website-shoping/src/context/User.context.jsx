6f3c12675c69354fdd60945eb3b9c611b33c8b3b
trinhhoangminh <hoangminh200401@gmail.com>
2025-07-22 18:30:25 +0700

    useReducee

--- website-shoping/src/context/User.context.jsx
+++ website-shoping/src/context/User.context.jsx
@@ -1,6 +1,7 @@
 import { createContext,useEffect, useReducer } from "react";
 // as the actual value you want to access 
 import { onAuthchangedListener, createUserDocAuth} from "../utils/firebase";
+import { createAction } from "../utils/reducer.utils";
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
@@ -13,9 +14,8 @@ export const UserProvider = ({ children }) => {
   // const [currentUser, setCurrentUser] = useState(null);
   const [state,dispatch] = useReducer(userReducer,INIT_STATE)
   const {currentUser}=state
-  console.log('current',currentUser)
   const setCurrentUser =(user)=>{
-    dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER,payload:user})
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   }
   const value = { currentUser, setCurrentUser}; 
  
@@ -35,8 +35,7 @@ export const USER_ACTION_TYPES={
     SET_CURRENT_USER:'SET_CURRENT_USER'
 }
 const userReducer = (state,action)=>{
-  console.log('dispatch')
-  console.log(action)
+
   const{type,payload}=action
   switch(type){
     case USER_ACTION_TYPES.SET_CURRENT_USER:
